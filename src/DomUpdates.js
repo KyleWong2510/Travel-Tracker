acfc15fffc1833a43ac889c3ae5007cbc099b2e8
Kyle Wong <kylewong2510@gmail.com>
2020-06-06 10:15:28 -0600

    Reorganize domUpdates and handler fns

--- src/DomUpdates.js
+++ src/DomUpdates.js
@@ -1,37 +1,32 @@
 import Agent from "./Agent"
+import moment from 'moment'
 
 const domUpdates = {
-  loadAgent(agent) {
-      document.getElementById('agent-trip-btn-container').classList.remove('hide')
-      document.getElementById('welcome-msg').innerText = 'Welcome, Agent'
-      document.getElementById('dollar-amt').innerText = `Annual Revenue: ${agent.annualRevenue}`
-      document.getElementById('search-input-title').innerText = 'Search Travelers'
-      document.getElementById('search-input').placeholder = 'Enter name...'
-      document.getElementById('main').classList.remove('hide')
-      document.getElementById('login').classList.add('hide')
+  date: moment().format('YYYY/MM/DD'),
+
+  loadAgent(agent, travelersData) {
+    document.getElementById('agent-trip-btn-container').classList.remove('hide')
+    document.getElementById('welcome-msg').innerText = 'Welcome, Agent'
+    document.getElementById('dollar-amt').innerText = `Annual Revenue: ${agent.calculateAnnualRevenue(travelersData)}`
+    document.getElementById('search-input-title').innerText = 'Search Travelers'
+    document.getElementById('search-input').placeholder = 'Enter name...'
+    document.getElementById('main').classList.remove('hide')
+    document.getElementById('login').classList.add('hide')
   },
   
   //do same as above
   loadTraveler() {
-    const usernameInput = document.getElementById('username-input')
-    const passwordInput = document.getElementById('password-input')
-    for (let i = 1; i < 51; i++) {
-      if(usernameInput.value === `traveler${i}` && passwordInput.value === 'travel2020') {
-        document.getElementById('traveler-trip-btn-container').classList.remove('hide')
-        document.getElementById('welcome-msg').innerText = 'Welcome, traveler'
-        document.getElementById('dollar-amt').innerText = 'Annual Amount Spent: '
-        document.getElementById('search-input-title').innerText = 'Search Destinations'
-        document.getElementById('search-input').placeholder = 'Enter Destination...'
-        document.getElementById('main').classList.remove('hide')
-        document.getElementById('login').classList.add('hide')
-      } 
-    }
-    if(!document.getElementById('login').classList.contains('hide') && usernameInput.value !== 'agency') {
-      alert('Sorry, unknown username')
-    }
+    document.getElementById('traveler-trip-btn-container').classList.remove('hide')
+    document.getElementById('welcome-msg').innerText = 'Welcome, traveler'
+    document.getElementById('dollar-amt').innerText = 'Annual Amount Spent: '
+    document.getElementById('search-input-title').innerText = 'Search Destinations'
+    document.getElementById('search-input').placeholder = 'Enter Destination...'
+    document.getElementById('main').classList.remove('hide')
+    document.getElementById('login').classList.add('hide')
   },
 
   displayTrips(trips) {
+    document.getElementById('main-content-results').innerHTML = ''
     trips.forEach(trip => {
       document.getElementById('main-content-results').insertAdjacentHTML('afterbegin', `
         <div id='trip-card'>
@@ -48,6 +43,21 @@ const domUpdates = {
         </div>
       `)
     })
+  },
+
+  filterAgentTrips(e, agent, tripsRepo) {
+    if(e.target.id === 'agent-pending') {
+      domUpdates.displayTrips(agent.getPendingTrips(tripsRepo))
+      document.getElementById('trip-title').innerText = 'Pending Trips'
+    }
+    if(e.target.id === 'agent-current') {
+      domUpdates.displayTrips(agent.getCurrentTrips(this.date, tripsRepo))
+      document.getElementById('trip-title').innerText = 'Current Trips'
+    }
+    if(e.target.id === 'agent-all') {
+      domUpdates.displayTrips(tripsRepo)
+      document.getElementById('trip-title').innerText = 'All Trips'
+    }
   }
 }
 
