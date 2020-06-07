d660dfbe75e15862936a74e31afb596769cfa972
Kyle Wong <kylewong2510@gmail.com>
2020-06-07 10:36:42 -0600

    Rearrange traveler dash elements

--- src/DomUpdates.js
+++ src/DomUpdates.js
@@ -18,9 +18,10 @@ const domUpdates = {
     document.getElementById('traveler-trip-btn-container').classList.remove('hide')
     document.getElementById('welcome-msg').innerText = `Welcome, ${traveler.name}`
     document.getElementById('dollar-amt').innerText = `Annual Amount Spent: ${traveler.annualCost}`
-    document.getElementById('search-input-title').innerText = 'Search Destinations'
+    document.getElementById('main-title').innerText = 'Search Destinations'
     document.getElementById('search-input').placeholder = 'Enter Destination...'
     this.displayDestinations(destinations)
+    this.displayTravelerPending(traveler, destinations)
     document.getElementById('main').classList.remove('hide')
     document.getElementById('login').classList.add('hide')
   },
@@ -50,18 +51,20 @@ const domUpdates = {
   },
 
   displayTravelerTrips(trips, destinationsData) {
-    document.getElementById('main-content-results').innerHTML = ''
+    document.getElementById('aside-results').innerHTML = ''
     trips.forEach(trip => {
-      document.getElementById('main-content-results').insertAdjacentHTML('afterbegin', `
+      document.getElementById('aside-results').insertAdjacentHTML('afterbegin', `
         <div id='trip-card'>
-          <div id='trip-card-ids'>
-          <p>${trip.getDestination(destinationsData).destination}</p>
-            <p>TripID: ${trip.id}</p>
-          </div>
-          <div id='trip-card-info'>
-            <p>Date: ${trip.date}</p>
-            <p>Duration: ${trip.duration}</p>
-            <p>Status: ${trip.status.toUpperCase()}</p>
+          <p id='trip-card-name'>${trip.getDestination(destinationsData).destination}</p>
+          <div id='trip-card-body'>
+            <div id='trip-card-ids'>
+              <p>TripID: ${trip.id}</p>
+              <p>Date: ${trip.date}</p>
+            </div>
+            <div id='trip-card-info'>
+              <p>Duration: ${trip.duration}</p>
+              <p>Status: ${trip.status.toUpperCase()}</p>
+            </div>
           </div>
         </div>
       `)
@@ -71,55 +74,56 @@ const domUpdates = {
   filterAgentTrips(e, agent, tripsRepo) {
     if (e.target.id === 'agent-pending') {
       this.displayAgentTrips(agent.getPendingTrips(tripsRepo))
-      document.getElementById('trip-title').innerText = 'Pending Trips'
+      document.getElementById('aside-title-text').innerText = 'Pending Trips'
     }
     if (e.target.id === 'agent-current') {
       this.displayAgentTrips(agent.getCurrentTrips(this.date, tripsRepo))
-      document.getElementById('trip-title').innerText = 'Current Trips'
+      document.getElementById('aside-title-text').innerText = 'Current Trips'
     }
     if (e.target.id === 'agent-all') {
       this.displayAgentTrips(tripsRepo)
-      document.getElementById('trip-title').innerText = 'All Trips'
+      document.getElementById('aside-title').innerText = 'All Trips'
     }
   },
 
   displayTravelerPending(traveler, destinationsData) {
+    document.getElementById('aside-title-text').innerText = 'Pending Trips'
     let pending = traveler.allTrips.filter(trip => trip.status === 'pending')
     if (pending.length === 0) {
-      document.getElementById('main-content-results').innerHTML = '<p>No trips awaiting approval</p>'
+      document.getElementById('aside-results').innerHTML = '<p>No trips awaiting approval</p>'
     } else {
       this.displayTravelerTrips(pending, destinationsData)
-      document.getElementById('trip-title').innerText = 'Pending Trips'
     }
   },
 
   displayTravelerPast(traveler, destinationsData) {
+    document.getElementById('aside-title-text').innerText = 'Past Trips'
     let past = traveler.filterTripsByDate(this.date, 'before')
     if (past.length === 0) {
-      document.getElementById('main-content-results').innerHTML = '<p>No past trips</p>'
+      document.getElementById('aside-results').innerHTML = '<p>No past trips</p>'
     } else {
       this.displayTravelerTrips(past, destinationsData)
-      document.getElementById('trip-title').innerText = 'Past Trips'
+      console.log(document.getElementById('aside-title-text'))
     }
   },
 
   displayTravelerCurrent(traveler, destinationsData) {
+    document.getElementById('aside-title-text').innerText = 'Current Trips'
     let current = traveler.getCurrentTrips(this.date)
     if (current.length === 0) {
-      document.getElementById('main-content-results').innerHTML = '<p>No current trips</p>'
+      document.getElementById('aside-results').innerHTML = '<p>No current trips</p>'
     } else {
       this.displayTravelerTrips(current, destinationsData)
-      document.getElementById('trip-title').innerText = 'Current Trips'
     }
   },
 
   displayTravelerUpcoming(traveler, destinationsData) {
+    document.getElementById('aside-title-text').innerText = 'Upcoming Trips'
     let upcoming = traveler.filterTripsByDate(this.date, 'after')
     if (upcoming.length === 0) {
-      document.getElementById('main-content-results').innerHTML = '<p>No upcoming trips</p>'
+      document.getElementById('aside-results').innerHTML = '<p>No upcoming trips</p>'
     } else {
       this.displayTravelerTrips(upcoming, destinationsData)
-      document.getElementById('trip-title').innerText = 'Upcoming Trips'
     }
   },
 
@@ -140,20 +144,24 @@ const domUpdates = {
 
   displayDestinations(destinationsRepo) {
     destinationsRepo.forEach(destination => {
-      document.getElementById('search-list').insertAdjacentHTML('beforeend', `
+      document.getElementById('main-content-results').insertAdjacentHTML('beforeend', `
         <div class='destination-card' id='${destination.id}'>
-          <h3 class='destination-title'>${destination.destination}</h3>
-          <img src='${destination.image}' alt='${destination.alt}'>
-          <p>Estimated Lodging: $${destination.estimatedLodgingCostPerDay} / day</p>
-          <p>Estimated Flight: $${destination.estimatedFlightCostPerPerson} / person</p>
-          <button class='plan-trip-btn'>Plan A Trip</button>
+          <div class='destination-info'>
+            <img src='${destination.image}' alt='${destination.alt}'>
+            <div class='destination-costs'>
+              <h3 class='destination-title'>${destination.destination}</h3>
+              <p>Estimated Lodging: $${destination.estimatedLodgingCostPerDay} / day</p>
+              <p>Estimated Flight: $${destination.estimatedFlightCostPerPerson} / person</p>
+              <button class='plan-trip-btn'>Plan A Trip</button>
+            </div>
+          </div>
         </div>
       `) 
     })
   },
 
   filterDestinations(destinationsRepo) {
-    document.getElementById('search-list').innerHTML = ''
+    document.getElementById('main-content-results').innerHTML = ''
     let search = document.getElementById('search-input')
     let searched = destinationsRepo.filter(destination => {
       return destination.destination.toLowerCase().includes(search.value.toLowerCase())
